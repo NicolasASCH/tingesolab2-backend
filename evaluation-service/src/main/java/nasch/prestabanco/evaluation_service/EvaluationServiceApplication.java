4d007699c1e577f60928c6b134eca26a6b65c40c
NicolasASCH <nicolas.saavedra.ch@usach.cl>
2024-12-10 09:43:59 -0300

    Cambios para que funcione

--- evaluation-service/src/main/java/nasch/prestabanco/evaluation_service/EvaluationServiceApplication.java
+++ evaluation-service/src/main/java/nasch/prestabanco/evaluation_service/EvaluationServiceApplication.java
@@ -2,10 +2,10 @@ package nasch.prestabanco.evaluation_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableEurekaClient
+@EnableDiscoveryClient
 public class EvaluationServiceApplication {
 
 	public static void main(String[] args) {
--- users-service/src/main/java/nasch/prestabanco/users_service/UsersServiceApplication.java
+++ users-service/src/main/java/nasch/prestabanco/users_service/UsersServiceApplication.java
@@ -2,10 +2,10 @@ package nasch.prestabanco.users_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableEurekaClient
+@EnableDiscoveryClient
 public class UsersServiceApplication {
 
 	public static void main(String[] args) {
